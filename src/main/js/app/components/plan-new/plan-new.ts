dbde0bd4f43e8813f005d5c954360807a3ebaaed
Xia Yingyin <xiayingyin@126.com>
2019-11-24 12:00:08 +0800

    fix some bugs

--- src/main/js/app/components/plan-new/plan-new.ts
+++ src/main/js/app/components/plan-new/plan-new.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from 'angular2/core';
+import { Component } from 'angular2/core';
 import { Router, ROUTER_DIRECTIVES } from 'angular2/router';
-import { Http, Response } from 'angular2/http';
 import { IPlan } from '../../interfaces/iplan';
 
 import { PlanService } from '../../services/plan-service';
-import { Observable } from 'rxjs/Observable';
-import { resolve } from 'url';
 import { SqlService } from '../../services/sql-service';
 
 @Component({
@@ -28,12 +25,12 @@ export class PlanNew {
   submitPlan() {
     this._sqlService.getQueryPlan(this.newPlanQuery).subscribe(res => {
       this.newPlanContent = res;
-      if (this.newPlanContent != null) {
+      if (this.newPlanContent !== null) {
         this.newPlanContent = this.newPlanContent.replace('QUERY PLAN=', '');
         this.newPlanContent = this.newPlanContent.slice(2, this.newPlanContent.length - 2);
 
         if (!this._planService.isJsonString(this.newPlanContent)) {
-          this.validationMessage = 'The string you submitted is not valid JSON'
+          this.validationMessage = 'The string you submitted is not valid JSON';
           return;
         }
 
--- src/main/js/app/services/sql-service.ts
+++ src/main/js/app/services/sql-service.ts
@@ -1,11 +1,11 @@
-import { Http, Response } from 'angular2/http';
+import { Http } from 'angular2/http';
 import { Injectable } from 'angular2/core';
 
 @Injectable()
 export class SqlService {
-    private queryURL: string = "http://localhost:8080/query/";
+    private queryURL: string = 'http://localhost:8080/query/';
     constructor(private _http: Http) { }
     getQueryPlan(query: string) {
-        return this._http.get(this.queryURL + query).map(response => response.text())
+        return this._http.get(this.queryURL + query).map(response => response.text());
     }
 }
