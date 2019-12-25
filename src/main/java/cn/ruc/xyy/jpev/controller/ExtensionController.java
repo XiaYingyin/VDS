871e9e2776f3e6c16c4d09e2d8e60ad197c81544
Xia Yingyin <xiayingyin@126.com>
2019-12-25 10:15:38 +0800

    add new restful api for extension and performance-opt

---
+++ src/main/java/cn/ruc/xyy/jpev/controller/ExtensionController.java
@@ -0,0 +1,58 @@
+package cn.ruc.xyy.jpev.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5555")
+public class ExtensionController {
+
+	public ExtensionController() {
+		// TODO Auto-generated constructor stub
+	}
+	@Autowired
+	private JdbcTemplate jdbc;
+
+	@RequestMapping(value ="/ext/list", method=RequestMethod.GET)
+	public String getQueryPlan(){
+		String ls_exts = "SELECT e.extname AS \"Name\", e.extversion AS \"Version\", n.nspname AS \"Schema\", c.description AS \"Description\" "
+				+ "FROM pg_catalog.pg_extension e LEFT JOIN pg_catalog.pg_namespace n ON n.oid = e.extnamespace LEFT JOIN pg_catalog.pg_description c ON c.objoid = e.oid AND " 
+				+ "c.classoid = 'pg_catalog.pg_extension'::pg_catalog.regclass";
+		try {
+			Object obj = jdbc.queryForList(ls_exts);
+			return obj.toString();
+		} catch (DataAccessException de) {
+			return "Exception: " + de;
+		}
+	}
+	
+	@RequestMapping(value = "/ext/install/{ext_name}", method=RequestMethod.GET)
+	public String installExt(@PathVariable("ext_name") String ext_name) {
+		String install = "create extension " + ext_name;
+		try {
+			jdbc.execute(install);
+			return "Install Extension " + ext_name + " Success!";
+		} catch (DataAccessException de) {
+			return "Exception: " + de;
+		}
+	}
+	
+	@RequestMapping(value = "/ext/uninstall/{ext_name}", method=RequestMethod.GET)
+	public String uninstallExt(@PathVariable("ext_name") String ext_name) {
+		String install = "drop extension " + ext_name;
+		try {
+			jdbc.execute(install);
+			return "Uninstall Extension " + ext_name + " Success!";
+		} catch (DataAccessException de) {
+			return "Exception: " + de;
+		}
+	}
+}
+
+
