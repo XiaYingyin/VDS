456b4d17d8bbdb6c261cd0567270bbdd75703c19
Xia Yingyin <xiayingyin@126.com>
2019-12-31 11:00:11 +0800

    make frontend as a submodule

--- src/main/js/app/components/about/about.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'about',
-    templateUrl: './components/about/about.html'
-})
-export class About {}
--- src/main/js/app/interfaces/iplan.ts
+++
@@ -1,9 +0,0 @@
-export interface IPlan {
-  id: string;
-  name: string;
-  content: any;
-  query: string;
-  createdOn: Date;
-  planStats: any;
-  formattedQuery: string;
-}
