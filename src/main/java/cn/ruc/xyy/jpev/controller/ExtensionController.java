849eb9bced31181183dbad8605a6c09a10cd212d
Xia Yingyin <xiayingyin@126.com>
2020-01-02 15:54:26 +0800

    adapt new frontend code
    * integrate new frontend
    * modified query controller to adapt new frontend
    * add devtools to make it auto restart

--- src/main/java/cn/ruc/xyy/jpev/controller/ExtensionController.java
+++ src/main/java/cn/ruc/xyy/jpev/controller/ExtensionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5555")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ExtensionController {
 
 	public ExtensionController() {
--- src/main/java/cn/ruc/xyy/jpev/controller/HomePageController.java
+++ src/main/java/cn/ruc/xyy/jpev/controller/HomePageController.java
@@ -1,10 +1,17 @@
 package cn.ruc.xyy.jpev.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomePageController {	
 	public HomePageController() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public String getHomePageInfo() {
+		System.out.println("test");
+		return "Hello, JPEV!";
 	}
 }
