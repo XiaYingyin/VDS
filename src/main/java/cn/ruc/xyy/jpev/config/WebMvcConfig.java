d2c70f3234ea571be0ac6baf11c186dea4d5602f
Liu Chaoyang <chaoyanglius@gmail.com>
2020-03-20 22:55:28 +0800

    mv config to right pos

--- src/main/java/cn/ruc/xyy/jpev/config/WebMvcConfig.java
+++ src/main/java/cn/ruc/xyy/jpev/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package cn.ruc.xyy.config;
+package cn.ruc.xyy.jpev.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
