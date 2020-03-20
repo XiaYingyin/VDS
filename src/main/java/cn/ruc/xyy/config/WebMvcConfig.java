86afb9d524c4b4d33b1d9e61c48bc78c76e80645
Liu Chaoyang <chaoyanglius@gmail.com>
2020-03-20 22:29:36 +0800

    add cors config

---
+++ src/main/java/cn/ruc/xyy/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package cn.ruc.xyy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowedOrigins("*").
+                allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS").
+                allowedHeaders("Content-Type","Access-Control-Allow-Headers","Authorization","X-Requested-With").
+                allowCredentials(true);
+    }
+}
