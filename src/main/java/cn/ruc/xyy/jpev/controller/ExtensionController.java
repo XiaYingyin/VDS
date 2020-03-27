5b7ee83324de1674c00d5aba951b92d5bb38aec9
Xia Yingyin <xiayingyin@126.com>
2020-03-27 13:36:38 +0800

    make test run on server

--- src/main/java/cn/ruc/xyy/jpev/controller/ExtensionController.java
+++ src/main/java/cn/ruc/xyy/jpev/controller/ExtensionController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class ExtensionController {
 
 	public ExtensionController() {
